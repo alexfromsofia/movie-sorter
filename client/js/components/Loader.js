032513834bc1dd46ffeef8a763ef3d8e0b80f11a
alexfromsofia <alexfromsofia@gmail.com>
2017-03-03 20:27:45 +0200

    loader is now a stateless component && added some css vars

--- client/js/components/Loader.js
+++ client/js/components/Loader.js
@@ -1,19 +1,13 @@
-import React, { Component, PropTypes } from 'react';
+import React from 'react';
 
-export default class Loader extends Component {
-    render() {
-        return (
-            <div className="loader">
-                <div className="rect1"></div>
-                <div className="rect2"></div>
-                <div className="rect3"></div>
-                <div className="rect4"></div>
-                <div className="rect5"></div>
-            </div>
-        );
-    }
-}
+const Loader = () => (
+    <div className="loader">
+        <div className="rect1"></div>
+        <div className="rect2"></div>
+        <div className="rect3"></div>
+        <div className="rect4"></div>
+        <div className="rect5"></div>
+    </div>
+);
 
-Loader.defaultProps = {};
-
-Loader.propTypes = {};
+export default Loader;
