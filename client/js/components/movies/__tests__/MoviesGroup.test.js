8f79b201bbd85be4069263a9053c7c9409de1ecb
alexfromsofia <alexfromsofia@gmail.com>
2017-03-03 10:58:49 +0200

    MoviesGroup tests

--- client/js/components/movies/__tests__/MoviesGroup.test.js
+++ client/js/components/movies/__tests__/MoviesGroup.test.js
@@ -4,9 +4,11 @@
 console.error = noop;
 /* eslint-enable no-console */
 import React from 'react';
+import Immutable from 'immutable';
 import { shallow } from 'enzyme';
+import { initialState } from '../../../reducers/movies';
 import { NoItems } from '../../../components';
-import { MoviesGroup } from '../../../components/movies';
+import { MoviesGroup, MoviesItem } from '../../../components/movies';
 import { noop } from '../../../utils/utils';
 
 const defaultProps = {
@@ -20,6 +22,38 @@ const defaultProps = {
 };
 const containerClassName = 'movie-items-group';
 const labelClassName = 'movie-items-label';
+const mockedMovies = [
+    {
+        id: 'pi_1998',
+        name: 'Pi',
+        year: 1998,
+        description: 'A young man who survives a disaster at sea is hurtled into an epic journey of adventure and discovery. While cast away, he forms an unexpected connection with another survivor: a fearsome Bengal tiger.',
+        isFavourite: true
+    },
+    {
+        id: 'armageddon_1998',
+        name: 'Armageddon',
+        year: 1998,
+        description: 'After discovering that an asteroid the size of Texas is going to impact Earth in less than a month, N.A.S.A. recruits a misfit team of deep core drillers to save the planet.',
+        isFavourite: false
+    },
+    {
+        id: 'heat_1995',
+        name: 'Heat',
+        year: 1995,
+        description: 'A group of professional bank robbers start to feel the heat from police when they unknowingly leave a clue at their latest heist.',
+        isFavourite: false
+    }
+];
+
+const mockedState = initialState.merge({
+    moviesById: {
+        [mockedMovies[0].id]: Immutable.fromJS(mockedMovies[0]),
+        [mockedMovies[1].id]: Immutable.fromJS(mockedMovies[1]),
+        [mockedMovies[2].id]: Immutable.fromJS(mockedMovies[2])
+    },
+    moviesIds: [mockedMovies[0].id, mockedMovies[1].id, mockedMovies[2].id]
+});
 
 describe('<MoviesGroup />', () => {
     it('should render a div', () => {
@@ -56,13 +90,39 @@ describe('<MoviesGroup />', () => {
         expect(received).toEqual(expected);
     });
 
+    it('it should render <MoviesItem />', () => {
+        const moviesIds = mockedState.get('moviesIds');
+        const moviesById = mockedState.get('moviesById');
+        const wrapper = shallow(
+            <MoviesGroup
+                moviesIds={moviesIds}
+                moviesById={moviesById}
+            />
+        );
+
+        expect(MoviesItem).toBeDefined();
+    });
+
+    it('it should render exact number of <MoviesItem />', () => {
+        const moviesIds = mockedState.get('moviesIds');
+        const moviesById = mockedState.get('moviesById');
+        const wrapper = shallow(
+            <MoviesGroup
+                moviesIds={moviesIds}
+                moviesById={moviesById}
+            />
+        );
+        const expected = moviesIds.size;
+        const received = wrapper.find('MoviesItem').length;
+
+        expect(received).toEqual(expected);
+    });
+
     describe('if no moviesIds exist', () => {
         it('should render <NoItems />', () => {
-            const wrapper = shallow(<MoviesGroup />);
-           // console.log(wrapper.debug())
+            shallow(<MoviesGroup />);
 
             expect(NoItems).toBeDefined();
         });
     });
-    //TODO: test if Component renders MoviesItem
 });
